49846a4e3e8317ee3864b1950445887c86daa320
Andrew Keller <Keller192@gmail.com>
2018-02-05 14:32:43 -0500

    adds documents array to be used, creates props to map over in List component and renders List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import List from './List';
 
+const documents = [
+  {
+    title: 'first doc',
+    content: 'one 1 one 1 one 1 one'
+  },
+  {
+    title: 'second doc',
+    content: 'two 2 two 2 two 2 two'
+  },
+  {
+    title: 'third doc',
+    content: 'three 3 three 3 three 3 three'
+  }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    }
+  }
+
   render() {
     return (
-      <div className="main">
-        <List />
-      </div>
+      <main>
+        <List 
+          listDocs={documents}
+        />
+      </main>
     );
   }
 }
--- src/List.js
+++ src/List.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const List = (props) => {
+
+    let titles = props.listDocs.map((doc, index) => {
+        return (
+            <div key={index} className="title">{doc.title}</div>
+        )
+    });
+
     return (
-        <div>
-            This is the list
+        <div className="title-list">
+            {titles}
         </div>
     );
 }
